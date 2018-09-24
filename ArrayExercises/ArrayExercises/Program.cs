36a26fac3c2ad001d3029e7e84cebab5ce8fa190
John Karaplis <johnkaraplis@Johns-MacBook-Pro.local>
2018-09-23 22:30:36 -0400

    first commit

---
+++ ArrayExercises/ArrayExercises/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+
+namespace ArrayExercises
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            // Given the array words[]
+            string[] words = new string[] { "chalk", "yarn", "barn", "born" };
+
+            // Display the word 'yarn'
+            Console.WriteLine("ANSWER HERE");
+
+            // Turn "yarn" into "jam" and display to console
+
+            // Turn "chalk" into "1" and display to console
+
+            // Turn "1" into "tarp" and display to console
+
+            // Create a new int[] called numbers of length 3 -- DO NOT create its elements
+
+            // Set the first element of numbers to 1 and display to console
+
+            // Set the third element of numbers to 2 and display to console
+
+            // Set the second element of numbers to 3 and display to console
+
+            // Using a for loop, display all elements of array numbers to console
+
+            // Using a while loop, display all elements of array numbers to console
+
+            // Using a for loop, display first three elements of words and all elements of numbers to console (using only one for block)
+
+            // Using a for loop, change numbers to reflect a correct sequential order of 1-2-3
+
+            // Display the second element of numbers
+
+            // Set the string variable thisWord to the last element of words
+            string thisWord = "";
+
+            // Display thisWord
+
+            // Display the last element of words
+
+            // Set the string variable thatWord to the first element of words
+            string thatWord = "";
+
+            // Set the first element of words equal to thisWord
+
+            // Display the first element of words
+
+            // Set the last element of words equal to thatWord
+
+            // Display the last element of words (should be "tarp")
+
+            // Display only the last letter of thatWord
+
+            // Multiply each element of array numbers by its square root
+
+            // Using a for loop, display each element within numbers
+
+            // Using a for loop, display only the middle element of numbers
+
+            // Using a for loop, display only the last element of words
+
+            // Using a for loop, display all elements of array words -- in reverse order
+
+            // Using a for loop, display only the middle two elements of words
+
+
+        }
+    }
+}
